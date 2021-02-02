8099c4b42fe612a60a1dcf82639c5d000fcd719b
Daria Szubierajska <daria.szubierajska@binarjobs.com>
2021-02-02 19:48:10 +0100

    product card set up

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        link: 'https://www2.hm.com/pl_pl/search-results.html?q=skarpety+on',
+        inStock: true,
+        onSale: true
+    }
+})
