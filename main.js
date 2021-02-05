ec3089ca83673a939a82f584ec7bd96266b08039
Daria Szubierajska <daria.szubierajska@binarjobs.com>
2021-02-05 22:25:15 +0100

    description component

--- main.js
+++ main.js
@@ -1,73 +1,88 @@
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+        <ul>
+            <li v-for="details in details">{{ detail }}</li>
+        </ul>
+    `
+})
+
 Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
 
     template: `
         <div class="product">
 
-            <div class="product-image">
-                <img :src="image">
-            </div>
+        <div class="product-image">
+        <img :src="image" />
+      </div>
 
-            <div class="product-info">
-                <h1>{{ title }}</h1>
-                <p v-if="inStock">In Stock</p>
-                <p v-else>Out of Stock</p>
-                <p>{{ sale }}</p>
+      <div class="product-info">
+          <h1>{{ product }}</h1>
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+          <p>Shipping: {{ shipping }}</p>
 
-                <ul>
-                    <li v-for="detail in details">{{ detail }}</li>
-                </ul>
+          <product-details :details="details"></product-details>
 
-                <ul>
-                    <li v-for="size in sizes">{{ size }}</li>
-                </ul>
+          <div class="color-box"
+               v-for="(variant, index) in variants" 
+               :key="variant.variantId"
+               :style="{ backgroundColor: variant.variantColor }"
+               @mouseover="updateProduct(index)"
+               >
+          </div> 
 
-                <div v-for="(variant, index) in variants"        
-                        :key="variant.variantId"
-                        class="color-box"
-                        :style="{ backgroundColor: variant.variantColor }"
-                        @mouseover="updateProduct(index)">
-                </div>
+          <button v-on:click="addToCart" 
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }"
+            >
+          Add to cart
+          </button>
 
-                <a :href="link" target="_blank">
-                    See more products like this
-                </a>
-            </div>
+          <div class="cart">
+            <p>Cart({{ cart }})</p>
+          </div>
 
-            <button @click="addToCart" 
-                    :disabled="!inStock"
-                    :class="{ disabledButton: !inStock }">Add to cart</button>
+       </div>  
 
-            <div class="cart">
-                <p>Cart({{ cart }})</p>
-            </div>
-        </div>
         </div>
     `,
     data() {
         return { 
-        brand: 'Awesome Vue',
-        product: 'Socks',
-        selectedVariant: 0,
-        link: 'https://www2.hm.com/pl_pl/search-results.html?q=skarpety+on',
-        details: ["60% cotton", "20% polyester", "femail product"],
-        variants: [
-            {
-               variantId: 2234,
-               variantColor: 'red',
-               variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-               variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
-                variantQuantity: 0
-            }
-        ],
-        sizes: ["34-35", "36-37", "38-39", "40-41"],
-        cart: 0,
-        onSale: true
-        
+            brand: 'Awesome Vue',
+            product: 'Socks',
+            selectedVariant: 0,
+            link: 'https://www2.hm.com/pl_pl/search-results.html?q=skarpety+on',
+            details: ["60% cotton", "20% polyester", "femail product"],
+            variants: [
+                {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+                variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 0
+                }
+            ],
+            sizes: ["34-35", "36-37", "38-39", "40-41"],
+            cart: 0,
+            onSale: true
+        } 
     },
     
     methods: {
@@ -96,11 +111,19 @@ Vue.component('product', {
                 return this.brand + ' ' + this.product + ' are on sale!'
             }
             return this.brand + ' ' + this.product + 'are not on sale.'
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free"
+            }
+            return 5.99
         }
     }
 })
 
-
 var app = new Vue({
     el: '#app',
+    data: {
+        premium: true
+    }
 })
