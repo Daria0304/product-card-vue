693da1dfe10262ed57306df6a87f42e9bb19a9c6
Daria Szubierajska <daria.szubierajska@binarjobs.com>
2021-02-06 18:45:11 +0100

    form wip

--- main.js
+++ main.js
@@ -23,45 +23,60 @@ Vue.component('product', {
     template: `
         <div class="product">
 
-        <div class="product-image">
-        <img :src="image" />
-      </div>
+            <div class="product-image">
+                <img :src="image" />
+            </div>
 
-      <div class="product-info">
-          <h1>{{ product }}</h1>
-          <p v-if="inStock">In Stock</p>
-          <p v-else>Out of Stock</p>
-          <p>Shipping: {{ shipping }}</p>
+            <div class="product-info">
+                <h1>{{ product }}</h1>
+                <p v-if="inStock">In Stock</p>
+                <p v-else>Out of Stock</p>
+                <p>Shipping: {{ shipping }}</p>
 
-          <product-details :details="details"></product-details>
+                <product-details :details="details"></product-details>
 
-          <div class="color-box"
-               v-for="(variant, index) in variants" 
-               :key="variant.variantId"
-               :style="{ backgroundColor: variant.variantColor }"
-               @mouseover="updateProduct(index)"
-               >
-          </div> 
+                <div class="color-box"
+                    v-for="(variant, index) in variants" 
+                    :key="variant.variantId"
+                    :style="{ backgroundColor: variant.variantColor }"
+                    @mouseover="updateProduct(index)"
+                    >
+                </div> 
 
-          <button v-on:click="addToCart" 
-            :disabled="!inStock"
-            :class="{ disabledButton: !inStock }"
-            >
-          Add to cart
-          </button>
+                <button @click="addToCart" 
+                    :disabled="!inStock"
+                    :class="{ disabledButton: !inStock }"
+                    >
+                Add to cart
+                </button>
 
-          <div class="cart">
-            <p>Cart({{ cart }})</p>
-          </div>
+                <button @click="removeFromCart" 
+                >
+                Remove from cart
+                </button>
 
-       </div>  
+                <div>
+                    <h2>Reviews</h2>
+                    <p v-if="!reviews.length">There are no reviews yet.</p>
+                    <ul>
+                        <li v-for="review in reviews">
+                        <p>{{ review.name }}</p>
+                        <p>Rating: {{ review.rating }}</p>
+                        <p>{{ review.review }}</p>
+                        </li>
+                    </ul>
+                </div>
+
+            </div> 
+            
+            <product-review @review-submitted="addReview"></product-review>
 
         </div>
     `,
     data() {
         return { 
             brand: 'Awesome Vue',
-            product: 'Socks',
+            product: 'Awesome Socks',
             selectedVariant: 0,
             link: 'https://www2.hm.com/pl_pl/search-results.html?q=skarpety+on',
             details: ["60% cotton", "20% polyester", "femail product"],
@@ -80,19 +95,25 @@ Vue.component('product', {
                 }
             ],
             sizes: ["34-35", "36-37", "38-39", "40-41"],
-            cart: 0,
-            onSale: true
+            onSale: true,
+            reviews: []
         } 
     },
     
     methods: {
         addToCart() {
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
         },
         updateProduct(index) {
             this.selectedVariant = index
             console.log(index);
             
+        },
+        removeFromCart() {
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
+        },
+        addReview(productReview) {
+            this.review.push(productReview)
         }
     },
 
@@ -121,9 +142,95 @@ Vue.component('product', {
     }
 })
 
+
+Vue.component('product-review', {
+    template: `
+    <form class="review-form"
+          @submit.prevent="onSubmit"
+        >
+
+        <p v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+            <ul>
+                <li v-for="error in errors">{{ error }}</li>
+            </ul>
+        </p>
+
+        <p>
+            <label for="name">Name:</label>
+            <input id="name" v-model="name">
+        </p>
+
+        <p>
+            <label for="review">Review:</label>
+            <textarea id="review" v-model="review"></textarea>
+        </p>
+
+        <p>
+            <label for="rating"">Rating:</label>
+            <select id="rating" v-model.number="rating">
+                <option>5</option>
+                <option>4</option>
+                <option>3</option>
+                <option>2</option>
+                <option>1</option>
+            </select>
+        </p>
+
+        <p>
+            <input type="submit" value="Submit">
+        </p>
+
+    </form>
+    `,
+    data() {
+        return {
+            name:null,
+            review: null,
+            rating: null,
+            errors: []
+        }
+    },
+
+    methods: {
+        onSubmit() {
+            if (this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null,
+                this.review = null,
+                this.rating = null
+            }
+            else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+            }
+            
+        }
+    }
+})
+
 var app = new Vue({
     el: '#app',
     data: {
-        premium: true
+        premium: true,
+        cart: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeItem(id) {
+            for(var i = this.cart.length - 1; i >= 0; i--) {
+                if(this.cart[i] === id) {
+                    this.cart.splice(i, 1);
+                }
+            }
+        }
     }
 })
